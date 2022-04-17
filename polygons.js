44bfc6b5c6d608dda01b9c9b71332acaa3067d05
Matthew Sivaprakasam <matthewjsiv@gmail.com>
2022-04-17 12:00:50 -0400

    initial push

---
+++ polygons.js
@@ -0,0 +1,100 @@
+var canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth+60;
+canvas.height = window.innerHeight+80;
+var ctx = canvas.getContext("2d");
+
+var TAU = 2 * Math.PI;
+
+times = [];
+function loop() {
+canvas.width = window.innerWidth+60;
+canvas.height = window.innerHeight+80;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  update();
+  draw();
+  requestAnimationFrame(loop);
+}
+
+function Ball (startX, startY, startVelX, startVelY) {
+  this.x = startX || Math.random() * canvas.width;
+  this.y = startY || Math.random() * canvas.height;
+  this.vel = {
+    x: startVelX || Math.random() * .6 - .3,
+    y: startVelY || Math.random() * .6 - .3
+  };
+  this.update = function(canvas) {
+    if (this.x > canvas.width + 50 || this.x < -50) {
+      this.vel.x = -this.vel.x;
+    }
+    if (this.y > canvas.height + 50 || this.y < -50) {
+      this.vel.y = -this.vel.y;
+    }
+    this.x += this.vel.x;
+    this.y += this.vel.y;
+  };
+  this.draw = function(ctx, can) {
+    ctx.beginPath();
+    ctx.globalAlpha = .1;
+    ctx.fillStyle = '#ffffff';
+    ctx.arc((0.5 + this.x) | 0, (0.5 + this.y) | 0, 3, 0, TAU, false);
+    ctx.fill();
+  }
+}
+
+var balls = [];
+for (var i = 0; i < canvas.width * canvas.height / (65*65); i++) {
+  balls.push(new Ball(Math.random() * canvas.width, Math.random() * canvas.height));
+}
+
+var lastTime = Date.now();
+function update() {
+  var diff = Date.now() - lastTime;
+  for (var frame = 0; frame * 16.6667 < diff; frame++) {
+    for (var index = 0; index < balls.length; index++) {
+      if(index % 2 == 0)
+      {
+      	balls[index].update(canvas);
+      	}
+      
+    }
+  }
+  lastTime = Date.now();
+}
+var mouseX = -1e9, mouseY = -1e9;
+document.addEventListener('mousemove', function(event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+});
+
+function distMouse(ball) {
+  return Math.hypot(ball.x - mouseX, ball.y - mouseY);
+}
+
+function draw() {
+  ctx.globalAlpha=1;
+  ctx.fillStyle = '#ffffff';
+  //ctx.fillStyle = '#fcf7f9';
+  //ctx.fillStyle = '#f2e8ec';
+  //ctx.fillStyle = '#f9f2f5';
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+  for (var index = 0; index < balls.length; index++) {
+    var ball = balls[index];
+    ball.draw(ctx, canvas);
+    ctx.beginPath();
+    for (var index2 = balls.length - 1; index2 > index; index2 += -1) {
+      var ball2 = balls[index2];
+      var dist = Math.hypot(ball.x - ball2.x, ball.y - ball2.y);
+        if (dist < 100) {
+          ctx.strokeStyle = "#c0e0f9";
+          ctx.globalAlpha = 1 - (dist > 100 ? .8 : dist / 150);
+          ctx.lineWidth = "60px";
+          ctx.moveTo((0.5 + ball.x) | 0, (0.5 + ball.y) | 0);
+          ctx.lineTo((0.5 + ball2.x) | 0, (0.5 + ball2.y) | 0);
+        }
+    }
+    ctx.stroke();
+  }
+}
+
+// Start
+loop();
